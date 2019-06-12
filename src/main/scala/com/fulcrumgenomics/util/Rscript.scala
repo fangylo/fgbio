e2325f2515d948db8bccf6945f3d9a084e6a4a1b
fangylo <fangyl43@gmail.com>
2019-06-11 19:06:15 -0700

    Refactored
    Refactored
    
    Refactored
    
    .

--- src/main/scala/com/fulcrumgenomics/util/Rscript.scala
+++ src/main/scala/com/fulcrumgenomics/util/Rscript.scala
@@ -24,49 +24,61 @@
 
 package com.fulcrumgenomics.util
 
+import java.lang.reflect
 import java.nio.file.Path
 
 import com.fulcrumgenomics.commons.util.LazyLogging
+import com.fulcrumgenomics.util.Rscript.IsModuleAvailable
 
-import scala.io.Source
 import scala.util.{Success, Try}
 
-/**
-  * Object that enables running of R scripts via the Rscript command line too.
-  */
-object Rscript extends LazyLogging {
-  /** The name of the Rscript executable. */
-  private val Executable = "Rscript"
+trait CommandLineTool extends LazyLogging {
+  /** The name of the executable such as Rscript or gs. */
+  val Executable: String
+
+  /** Suffix for scripts that can be run by this tool. */
+  val Suffix: String
+
+  /** Command used for the above executable. */
+  val TestCommand: Seq[String]
+
+  /** Args used to specify expression of the tool, e,g, "-e" in `Rscript -e "require(ggplot2)`. */
+  val Expression_arg: String = null
 
   /** Exception class that holds onto an executable's exit/status code. */
-  case class RscriptException(status: Int) extends RuntimeException {
-    override def getMessage: String = s"Rscript failed with exit code ${status}."
+  case class ToolException(status: Int) extends RuntimeException {
+    override def getMessage: String = s"$Executable failed with exit code $status."
   }
 
-  /** Returns true if R is available and false otherwise. */
-  lazy val Available: Boolean = {
+  /** Returns true if the command can execute with no error. */
+  def canExecute(command: String*): Boolean = {
     try {
-      val process = new ProcessBuilder(Executable, "-e", "require(ggplot2)").redirectErrorStream(true).start()
+      val process = new ProcessBuilder(command: _*).redirectErrorStream(true).start()
       process.waitFor() == 0
     }
     catch { case e: Exception => false }
   }
 
-  /** Executes an Rscript from the classpath if Rscript is available. */
+  /** Returns true if the tool is available and false otherwise. */
+  lazy val Available: Boolean = {
+    canExecute(Executable +: TestCommand:_*)
+  }
+
+  /** Executes a script from the classpath if the tested executable is available. */
   def execIfAvailable(scriptResource: String, args: String*): Try[Unit] =
     if (Available) exec(scriptResource, args:_*) else Success(Unit)
 
-  /** Executes an Rscript from a script stored at a Path if Rscript is available. */
+  /** Executes from a script stored at a Path if the tested executable is available. */
   def execIfAvailable(script: Path, args: String*): Try[Unit] =
     if (Available) exec(script, args:_*) else Success(Unit)
 
-  /** Executes an Rscript from the classpath. */
+  /** Executes a script from the classpath. */
   def exec(scriptResource: String, args: String*): Try[Unit] =
     Try { writeResourceToTempFile(scriptResource) }.map(path => exec(path, args:_*))
 
-  /** Executes an Rscript from a script stored at a Path. */
+  /** Executes from a script stored at a Path. */
   def exec(script: Path, args: String*): Try[Unit] = Try {
-    val command = Rscript.Executable +: script.toAbsolutePath.toString +: args
+    val command = Executable +: script.toAbsolutePath.toString +: args
     val process = new ProcessBuilder(command:_*).redirectErrorStream(false).start()
     val pipe1   = Io.pipeStream(process.getErrorStream, logger.info)
     val pipe2   = Io.pipeStream(process.getInputStream, logger.debug)
@@ -74,15 +86,64 @@ object Rscript extends LazyLogging {
     pipe1.close()
     pipe2.close()
 
-    if (retval != 0) throw RscriptException(retval)
+    if (retval != 0) throw ToolException(retval)
   }
 
   /** Extracts a resource from the classpath and writes it to a temp file on disk. */
   private def writeResourceToTempFile(resource: String): Path = {
     val lines = Io.readLinesFromResource(resource).toSeq
-    val path = Io.makeTempFile("script.", ".R")
+    val path = Io.makeTempFile("script.", suffix = Suffix)
     path.toFile.deleteOnExit()
     Io.writeLines(path, lines)
     path
   }
 }
+
+trait Versioned {
+  // Test version of the tool
+  self: CommandLineTool =>
+  val VersionFlag: String      = "--version"
+  val TestCommand: Seq[String] = Seq(VersionFlag)
+}
+
+/** Test if specified module(s) are included with the tested executable. */
+trait Modular {
+  self: CommandLineTool =>
+  def TestModuleCommand(module: String): Seq[String] = Seq(Executable, Expression_arg, module)
+  def TestModuleCommand(modules: Seq[String]): Seq[Seq[String]] = modules.map(TestModuleCommand)
+  def IsModuleAvailable(module: String): Boolean =
+    canExecute(TestModuleCommand(module): _*)
+  def IsModuleAvailable(modules: Seq[String]): Boolean =
+    // Only returns true is all modules exist
+    modules.map(IsModuleAvailable).forall(x => x == true)
+}
+
+object Rscript extends CommandLineTool with Versioned with Modular {
+  val Executable: String      = "Rscript"
+  val Suffix: String          = ".R"
+  override val Expression_arg = "-e"
+  override lazy val Available: Boolean = {
+    // Only returns true if R executable exists and ggplot2 is installed
+    val ToolAvailable: Boolean = canExecute(Executable +: Seq(VersionFlag):_*)
+    val ModuleAvailable : Boolean = IsModuleAvailable(module = "library(ggplot2)")
+    Seq(ToolAvailable,ModuleAvailable).forall(_==true)
+  }
+//    println("--------------11111111111------------")
+//    println("Executable:"+ Executable)
+//    println("TestCommand:" + TestCommand) // Should just be testing version here
+//    println("Tool Available:" + Available.toString)
+}
+
+object GhostScript extends CommandLineTool with Versioned {
+  val Executable: String = "gs"
+  val Suffix: String = ".sh"
+}
+
+
+//object Python3 extends CommandLineTool with Versioned with Modular {
+//  val Executable: String = "python3"
+//  val Suffix: String = ".py"
+//  override val Expression_arg = "-c"
+//  override val TestCommand: Seq[String] = Seq(VersionFlag)
+//  val ModuleAvailable : Boolean = ModuleAvailable(module = "import numpy")
+//}
